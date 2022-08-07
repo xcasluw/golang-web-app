733d1549aa6620c5bceef4ec43c58c40aee13b3c
Lucas Genari <oligena@hotmail.com>
2022-08-06 23:17:04 -0300

    using lib chi for routing

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,15 +3,18 @@ package main
 import (
 	"net/http"
 
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/xcasluw/golang-basic-app/pkg/config"
 	"github.com/xcasluw/golang-basic-app/pkg/handlers"
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	mux := pat.New()
+	mux := chi.NewRouter()
 
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	mux.Use(middleware.Recoverer)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
 	return mux
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/xcasluw/golang-basic-app
 
 go 1.13
 
-require github.com/bmizerany/pat v0.0.0-20210406213842-e4b6760bdd6f // indirect
+require github.com/go-chi/chi/v5 v5.0.7
