7366aae4d8dc39a1b9199d8af9ecf7ff23a8461c
Lucas Genari <oligena@hotmail.com>
2022-08-09 21:26:16 -0300

    server side form validation iii

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -56,8 +56,13 @@ func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 
 // Reservations renders the make a reservation page and displays form
 func (m *Repository) Reservations(w http.ResponseWriter, r *http.Request) {
+	var emptyReservation models.Reservation
+	data := make(map[string]interface{})
+	data["reservation"] = emptyReservation
+
 	render.RenderTemplate(w, r, "make-reservation.page.tmpl", &models.TemplateData{
 		Form: forms.New(nil),
+		Data: data,
 	})
 }
 
@@ -77,11 +82,8 @@ func (m *Repository) PostReservations(w http.ResponseWriter, r *http.Request) {
 	}
 
 	form := forms.New(r.PostForm)
-
-	form.Has("first_name", r)
-	form.Has("last_name", r)
-	form.Has("email", r)
-	form.Has("phone", r)
+	form.Required("first_name", "last_name", "email")
+	form.MinLength("first_name", 3, r)
 
 	if !form.Valid() {
 		data := make(map[string]interface{})
